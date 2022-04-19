ce684f1b96d9fa6c8a789c63ea752b6e28f52fda
Tan Ching Hung <103587066+chinghung2000@users.noreply.github.com>
2022-04-19 23:59:26 +0800

    Add files via upload

--- src/com/project/backend/LecturerInterface.java
+++ src/com/project/backend/LecturerInterface.java
@@ -1,5 +1,23 @@
 package com.project.backend;
 
 public interface LecturerInterface {
-
+	void getAllWorkloads(int lecturerId);
+	
+	void getAllQuizTF(int workloadId);
+	boolean addQuizTF(int workloadId, String question, boolean answer, int modifiedBy);
+	boolean updateQuizTF(int quizTFId, String question, boolean answer, int modifiedBy);
+	boolean deleteQuizTF(int quizTFId);
+	void getAllQuizTFResults(int workloadId);
+	
+	void getAllQuizObj(int workloadId);
+	boolean addQuizObj(int workloadId, String question, String choiceA, String choiceB, String choiceC, String choiceD, char answer, int modifiedBy);
+	boolean updateQuizObj(int quizObjId, String question, String choiceA, String choiceB, String choiceC, String choiceD, char answer, int modifiedBy);
+	boolean deleteQuizObj(int quizObjId);
+	void getAllQuizObjResults(int workloadId);
+	
+	void getAllTasks(int workloadId);
+	boolean addTask(int workloadId, String taskName, String fileName, int modifiedBy);
+	boolean updateTask(int taskId, int workloadId, String taskName, String fileName, int modifiedBy);
+	boolean deleteTask(int taskId);
+	void getAllSubmissions(int taskId);
 }
--- src/com/project/backend/StudentInterface.java
+++ src/com/project/backend/StudentInterface.java
@@ -1,5 +1,18 @@
 package com.project.backend;
 
 public interface StudentInterface {
-
+	void getAllRegisteredSubjects(String studentId);
+	boolean addRegisteredSubject(String studentId, int workloadId);
+	boolean deleteRegisteredSubject(int registeredSubjectId);
+	
+	void getAllQuizTF();
+	boolean updateQuizTFMark(int registeredSubjectId, int quizTFMark);
+	
+	void getAllQuizObj();
+	boolean updateQuizObjMark(int registeredSubjectId, int quizObjMark);
+	
+	void getAllTasks(int workloadId);
+	void getAllSubmissions(int taskId, String studentId);
+	boolean addSubmission(int taskId, String studentId, String fileName);
+	boolean deleteSubmission(int submissionId);
 }
