670141b540008cdab5beadf4092ae3dedc5450b7
Ching Hung Tan <103587066+chinghung2000@users.noreply.github.com>
2022-05-19 18:08:21 +0800

    Add files via upload

--- src/com/project/backend/DatabaseManager.java
+++ src/com/project/backend/DatabaseManager.java
@@ -1,9 +1,67 @@
+/*
+ * Database Manager 2.3.2
+ * ~~~~~~~~~~~~~~~~~~~~~~
+ * 
+ * Database Manager is written in Java, for shorten the code in programming.
+ * Provides better database implementation when coding with Java.
+ * 
+ * Database Manager can handle SQL statement preparation with flexible
+ * parameter binding. Say bye to manually preparing SQL statements and binds
+ * parameters to SQL statements one by one with corresponding data types.
+ * Provides error handling, as well as event logging to make debugging easier.
+ * 
+ * 
+ * Basic usage:
+ * ... create DatabaseManager object:
+ * 
+ *     +----------------------------------------------------------------------------------------+
+ *     |																						|
+ *     |	Connection databaseConnection = null;												|
+ *     |																						|
+ *     |	try {																				|
+ *     |		databaseConnection = DriverManager.getConnection(url, username, password);		|
+ *     |	} catch (SQLException e) {															|
+ *     |		e.printStackTrace();															|
+ *     |	}																					|
+ *     |																						|
+ *     |	DatabaseManager db = new DatabaseManager(databaseConnection);						|
+ *     |																						|
+ *     +----------------------------------------------------------------------------------------+
+ * 
+ * ... or prepare a statement:
+ * 
+ *     +----------------------------------------------------------------------------------------+
+ *     |																						|
+ *     |	int adminId = 1;																	|
+ *     |	db.prepare("SELECT * FROM admin WHERE admin_id = ?;", adminId);						|
+ *     |																						|
+ *     +----------------------------------------------------------------------------------------+
+ * 
+ * ... or execute a query with returning results:
+ * 
+ *     +----------------------------------------------------------------------------------------+
+ *     |																						|
+ *     |	ResultSet rs = db.executeQuery();													|
+ *     |	String adminName = rs.getString("admin_name");										|
+ *     |																						|
+ *     +----------------------------------------------------------------------------------------+
+ * 
+ * 
+ * 
+ * Copyright (c) 2022 BotBox Studio. All rights reserved.
+ * Version: 2.3.2
+ * Last updated on 19/05/2022, 10:04:08 UTC
+ * Author: Ching Hung Tan
+ * GitHub: chinghung2000
+ * Email: tanchinghung5098.1@gmail.com
+ */
+
 package com.project.backend;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Date;
 import java.sql.SQLException;
 
 public class DatabaseManager {
@@ -19,7 +77,8 @@ public class DatabaseManager {
 	public boolean prepare(String statement, Object... parameters) {
 		if (this.connection != null) {
 			try {
-				this.pstmt = this.connection.prepareStatement(statement, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				this.pstmt = this.connection.prepareStatement(statement, ResultSet.TYPE_SCROLL_INSENSITIVE,
+						ResultSet.CONCUR_READ_ONLY);
 				System.out.println("DatabaseManager.prepare: Preparing SQL statement: \"" + statement + "\"...");
 				int i = 1;
 
@@ -49,7 +108,7 @@ public class DatabaseManager {
 
 				return true;
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.prepare: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.prepare: There is an error: " + e.toString());
 			}
 		} else {
 			System.out.println("DatabaseManager.prepare: No database connection");
@@ -58,12 +117,13 @@ public class DatabaseManager {
 		return false;
 	}
 
-	// to prepare a SQL statement and bind respective parameters for returning generated keys
+	// to prepare a SQL statement and bind respective parameters for returning
+	// generated keys
 	public boolean prepareForGeneratedKey(String statement, Object... parameters) {
 		if (this.connection != null) {
 			try {
 				this.pstmt = this.connection.prepareStatement(statement, PreparedStatement.RETURN_GENERATED_KEYS);
-				System.out.println("DatabaseManager.prepare: Preparing SQL statement: \"" + statement + "\"...");
+				System.out.println("DatabaseManager.prepareForGeneratedKey: Preparing SQL statement: \"" + statement + "\"...");
 				int i = 1;
 
 				for (Object parameter : parameters) {
@@ -90,10 +150,10 @@ public class DatabaseManager {
 
 				return true;
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.prepare: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.prepareForGeneratedKey: There is an error: " + e.toString());
 			}
 		} else {
-			System.out.println("DatabaseManager.prepare: No database connection");
+			System.out.println("DatabaseManager.prepareForGeneratedKey: No database connection");
 		}
 
 		return false;
@@ -108,9 +168,10 @@ public class DatabaseManager {
 				int rowCount = rs.last() ? rs.getRow() : 0;
 				rs.beforeFirst();
 				System.out.println("DatabaseManager.executeQuery: " + rowCount + " row(s) found.");
+
 				return rs;
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.executeQuery: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.executeQuery: There is an error: " + e.toString());
 			}
 		} else {
 			System.out.println("DatabaseManager.executeQuery: No database connection");
@@ -126,9 +187,10 @@ public class DatabaseManager {
 				System.out.println("DatabaseManager.executeUpdate: Executing prepared SQL statement...");
 				int affectedRowCount = this.pstmt.executeUpdate();
 				System.out.println("DatabaseManager.executeUpdate: " + affectedRowCount + " row(s) affected.");
+
 				return true;
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.executeUpdate: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.executeUpdate: There is an error: " + e.toString());
 			}
 		} else {
 			System.out.println("DatabaseManager.executeUpdate: No database connection");
@@ -137,20 +199,21 @@ public class DatabaseManager {
 		return false;
 	}
 
-	// to execute SQL statement without returning result for returning generated keys
+	// to execute SQL statement without returning result for returning generated
+	// keys
 	public ResultSet executeUpdateForGeneratedKey() {
 		if (this.pstmt != null) {
 			try {
-				System.out.println("DatabaseManager.executeUpdate: Executing prepared SQL statement...");
+				System.out.println("DatabaseManager.executeUpdateForGeneratedKey: Executing prepared SQL statement...");
 				int affectedRowCount = this.pstmt.executeUpdate();
-				System.out.println("DatabaseManager.executeUpdate: " + affectedRowCount + " row(s) affected.");
-				
+				System.out.println("DatabaseManager.executeUpdateForGeneratedKey: " + affectedRowCount + " row(s) affected.");
+
 				return this.pstmt.getGeneratedKeys();
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.executeUpdate: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.executeUpdateForGeneratedKey: There is an error: " + e.toString());
 			}
 		} else {
-			System.out.println("DatabaseManager.executeUpdate: No database connection");
+			System.out.println("DatabaseManager.executeUpdateForGeneratedKey: No database connection");
 		}
 
 		return null;
@@ -162,9 +225,10 @@ public class DatabaseManager {
 			try {
 				this.connection.close();
 				System.out.println("DatabaseManager.close: Database connection closed.");
+
 				return true;
 			} catch (SQLException e) {
-				System.out.println("DatabaseManager.close: There are some errors: " + e.toString());
+				System.out.println("DatabaseManager.close: There is an error: " + e.toString());
 			}
 		} else {
 			System.out.println("DatabaseManager.close: No database connection");
---
+++ src/com/project/backend/FileChecksum.java
@@ -0,0 +1,45 @@
+package com.project.backend;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class FileChecksum {
+	public static final String MD2 = "MD2";
+	public static final String MD5 = "MD5";
+	public static final String SHA = "SHA";
+	public static final String SHA_256 = "SHA-256";
+	public static final String SHA_384 = "SHA-384";
+	public static final String SHA_512 = "SHA-512";
+
+	@SuppressWarnings("unused")
+	private static String checksum(String filePath, String algorithm) {
+		MessageDigest messageDigest = null;
+
+		// file hashing with DigestInputStream
+		try {
+			messageDigest = MessageDigest.getInstance(algorithm);
+			DigestInputStream digestInputStream = new DigestInputStream(new FileInputStream(filePath), messageDigest);
+			while (digestInputStream.read() != -1);	// empty loop to clear the data
+			messageDigest = digestInputStream.getMessageDigest();
+			digestInputStream.close();
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("FileChecksum: There are an error: " + e.toString());
+			return null;
+		} catch (IOException e) {
+			System.out.println("FileChecksum: There are an error: " + e.toString());
+			return null;
+		}
+
+		// bytes to hex
+		StringBuilder result = new StringBuilder();
+
+		for (byte b : messageDigest.digest()) {
+			result.append(String.format("%02x", b));
+		}
+
+		return result.toString();
+	}
+}
